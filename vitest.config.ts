51028ef1a92c3f5a97928cb02e17129287b6ba53
avallete <andrew.valleteau@supabase.io>
2025-09-10 13:42:24 +0200

    chore: configure coverage to only consider integration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vitest/config";
 
+const testIntegrationsInclude = [
+  "**/*.integration.test.ts",
+  "tests/integration/**/*.test.ts",
+];
+
 export default defineConfig({
   test: {
     globalSetup: ["./tests/global-setup.ts"],
     testTimeout: 60_000,
     slowTestThreshold: 10_000,
+    coverage: {
+      // Also report coverage if some tests fail
+      reportOnFailure: true,
+      // Only consider integrations tests for coverage as their the only ones
+      // ensuring real e2e working behaviour
+      include: testIntegrationsInclude,
+    },
     projects: [
       {
         extends: true,
@@ -22,10 +34,7 @@ export default defineConfig({
           // Integration tests - run with single fork to share containers
           name: "integration",
           retry: 0,
-          include: [
-            "**/*.integration.test.ts",
-            "tests/integration/**/*.test.ts",
-          ],
+          include: testIntegrationsInclude,
           pool: "forks",
           poolOptions: {
             forks: {
