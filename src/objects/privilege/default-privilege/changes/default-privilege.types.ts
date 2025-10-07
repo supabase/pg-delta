65329707d9ce025ba961c24b1fa45913998be937
Julien Goux <hi@jgoux.dev>
2025-10-07 10:48:34 +0200

    delete privilege folder

--- src/objects/privilege/default-privilege/changes/default-privilege.types.ts
+++
@@ -1,3 +0,0 @@
-import type { AlterDefaultPrivilege } from "./default-privilege.alter.ts";
-
-export type DefaultPrivilegeChange = AlterDefaultPrivilege;
--- src/objects/privilege/privilege.types.ts
+++
@@ -1,3 +0,0 @@
-import type { DefaultPrivilegeChange } from "./default-privilege/changes/default-privilege.types.ts";
-
-export type PrivilegeChange = DefaultPrivilegeChange;
